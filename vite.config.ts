f7f78e865feaea3f9bddf5b62a74832b42f28395
ayoakouh <akouharayoub12@gmail.com>
2026-07-07 13:58:28 +0100

    pleas

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 5173, strictPort: true },
+  base: "/",
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: { port: 5173, strictPort: true },
+// });
